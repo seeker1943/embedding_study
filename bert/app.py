7609c56297c22d636b46a30d206ec1ed0e1c3fc2
baili <yucong@mail.i-i.ai>
2019-04-17 21:08:47 +0800

    add classification

--- bert/app.py
+++ bert/app.py
@@ -4,7 +4,8 @@
 @author: Cong Yu
 @time: 2018/12/6 7:28 PM
 """
-import os,time
+import os, time, sys
+sys.path.append("../")
 import tensorflow as tf
 from tensorflow.python.estimator.estimator import Estimator
 from tensorflow.python.estimator.run_config import RunConfig
@@ -13,7 +14,7 @@ from bert.extract_features import model_fn_builder, convert_lst_to_features, Poo
 
 # 获取当前文件的上层路径
 path = os.path.dirname(os.path.abspath(__file__))
-model_dir = "/Users/yucong/PycharmProjects/embedding_bert/chinese_L-12_H-768_A-12"
+model_dir = "/Users/yucong/PycharmProjects/helloAi/bert"
 config_fp = os.path.join(model_dir, 'bert_config.json')
 checkpoint_fp = os.path.join(model_dir, 'bert_model.ckpt')
 vocab_fp = os.path.join(model_dir, 'vocab.txt')
@@ -30,7 +31,8 @@ model_fn = model_fn_builder(
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
-estimator = Estimator(model_fn, config=RunConfig(session_config=config),model_dir=None)
+estimator = Estimator(model_fn, config=RunConfig(session_config=config), model_dir=None)
+
 
 def input_fn_builder(msg):
     def gen():
@@ -58,8 +60,9 @@ def input_fn_builder(msg):
 
     return input_fn
 
+
 t1 = time.time()
-input_fn = input_fn_builder(["NLP好难啊！","怎么办呢？"])
+input_fn = input_fn_builder(["NLP好难啊！", "怎么办呢？"])
 
 result = estimator.predict(input_fn)
 for rq in result:
@@ -67,4 +70,4 @@ for rq in result:
     print(rq['encodes'])
 t2 = time.time()
 print(a.shape)
-print("cost time:",t2-t1)
+print("cost time:", t2 - t1)
